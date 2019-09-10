8afa9f54d9d66c949a3fa1ae59861a0d886fee27
Fandrelo <14915329+Fandrelo@users.noreply.github.com>
2019-09-10 15:38:25 -0600

    Add project files.

---
+++ AspNetCoreDbSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreDbSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
